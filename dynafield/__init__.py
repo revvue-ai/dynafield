385a70a04df878e8bea2d1c58090148d0ecd4f27
BadrElfarri <47941400+BadrElfarri@users.noreply.github.com>
2025-10-07 15:15:11 +0200

    Refine record schema naming and registry support

--- dynafield/__init__.py
+++ dynafield/__init__.py
@@ -1,52 +1,22 @@
-from typing import Annotated, Union
-
-from pydantic import Field
-
 from dynafield.fields.base_field import DataTypeFieldBase, FieldTypeEnum
-from dynafield.fields.bool_field import BoolField, BoolFieldGql
-from dynafield.fields.date_field import DateField, DateFieldGql, DateTimeField, DateTimeFieldGql
-from dynafield.fields.email_field import EmailField, EmailFieldGql
-from dynafield.fields.enum_field import EnumField, EnumFieldGql
-from dynafield.fields.float_field import FloatField, FloatFieldGql
-from dynafield.fields.int_field import IntField, IntFieldGql
-from dynafield.fields.json_field import JsonField, JsonFieldGql
-from dynafield.fields.list_field import ListField, ListFieldGql
-from dynafield.fields.object_field import ObjectField, ObjectFieldGql
-from dynafield.fields.str_field import StrField, StrFieldGql
-from dynafield.fields.uuid_field import UuidField, UuidFieldGql
+from dynafield.fields.bool_field import BoolField
+from dynafield.fields.date_field import DateField, DateTimeField
+from dynafield.fields.email_field import EmailField
+from dynafield.fields.enum_field import EnumField
+from dynafield.fields.float_field import FloatField
+from dynafield.fields.int_field import IntField
+from dynafield.fields.json_field import JsonField
+from dynafield.fields.list_field import ListField
+from dynafield.fields.object_field import ObjectField
+from dynafield.fields.str_field import StrField
+from dynafield.fields.uuid_field import UuidField
 from dynafield.from_func import build_dynamic_model, build_model_from_function, fields_from_function
-
-TypeFieldsUnionGql = (
-    BoolFieldGql
-    | DateFieldGql
-    | DateTimeFieldGql
-    | EmailFieldGql
-    | EnumFieldGql
-    | FloatFieldGql
-    | IntFieldGql
-    | JsonFieldGql
-    | ListFieldGql
-    | StrFieldGql
-    | UuidFieldGql
-    | ObjectFieldGql
+from dynafield.record_schema import (
+    RecordSchemaDefinition,
+    RecordSchemaRegistry,
+    TypeFieldsUnion,
+    TypeFieldsUnionGql,
 )
-TypeFieldsUnion = Annotated[
-    Union[
-        "BoolField",
-        "DateField",
-        "DateTimeField",
-        "EmailField",
-        "EnumField",
-        "FloatField",
-        "IntField",
-        "JsonField",
-        "ListField",
-        "StrField",
-        "UuidField",
-        "ObjectField",
-    ],
-    Field(discriminator="typename__"),
-]
 
 __all__ = [
     "build_model_from_function",
@@ -66,4 +36,8 @@ __all__ = [
     "StrField",
     "UuidField",
     "ObjectField",
+    "RecordSchemaDefinition",
+    "RecordSchemaRegistry",
+    "TypeFieldsUnion",
+    "TypeFieldsUnionGql",
 ]
