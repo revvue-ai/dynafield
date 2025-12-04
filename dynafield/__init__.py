1e35aa6e56b5d9fa153a2bc28f3611afa7ab90b0
BadrElfarri <47941400+BadrElfarri@users.noreply.github.com>
2025-12-04 01:07:35 +0100

    Fix dynafield package imports

--- dynafield/__init__.py
+++ dynafield/__init__.py
@@ -1,18 +1,18 @@
-from dynafield import amqp, clerk, database, expressions, gql, logger, models, tracing, utils
-from dynafield.fields.base_field import DataTypeFieldBase, FieldTypeEnum, build_dynamic_model
-from dynafield.fields.bool_field import BoolField
-from dynafield.fields.date_field import DateField, DateTimeField
-from dynafield.fields.email_field import EmailField
-from dynafield.fields.enum_field import EnumField
-from dynafield.fields.float_field import FloatField
-from dynafield.fields.int_field import IntField
-from dynafield.fields.json_field import JsonField
-from dynafield.fields.list_field import ListField
-from dynafield.fields.object_field import ObjectField
-from dynafield.fields.str_field import StrField
-from dynafield.fields.uuid_field import UuidField
-from dynafield.from_func import build_model_from_function, fields_from_function
-from dynafield.record_schema import (
+from . import amqp, clerk, database, expressions, gql, logger, models, tracing, utils
+from .fields.base_field import DataTypeFieldBase, FieldTypeEnum, build_dynamic_model
+from .fields.bool_field import BoolField
+from .fields.date_field import DateField, DateTimeField
+from .fields.email_field import EmailField
+from .fields.enum_field import EnumField
+from .fields.float_field import FloatField
+from .fields.int_field import IntField
+from .fields.json_field import JsonField
+from .fields.list_field import ListField
+from .fields.object_field import ObjectField
+from .fields.str_field import StrField
+from .fields.uuid_field import UuidField
+from .from_func import build_model_from_function, fields_from_function
+from .record_schema import (
     RecordSchemaDefinition,
     RecordSchemaRegistry,
     TypeFieldsUnion,
